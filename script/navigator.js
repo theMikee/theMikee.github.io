88f602cdeee381bb15d2135cfcd1c1b61be63f48
Mikee <grigoremihai589@gmail.com>
2025-05-31 21:06:16 +0300

    now working

--- script/navigator.js
+++ script/navigator.js
@@ -1,38 +1,38 @@
 import { probleme } from "./probleme.js";
 import { highlightSyntax } from "./codeHighlighter.js";
 
-let back = document.getElementById("");
-let fwd = document.getElementById("");
-let txtLabel = document.getElementById("");
+// let back = document.getElementById("");
+// let fwd = document.getElementById("");
+// let txtLabel = document.getElementById("");
 
 let curent = 0;
-let rawCode = "";
+let rawCode = probleme[0].text;
 
-back.addEventListener("click", function()
-{
-    curent--;
-    if(current == 0)
-    {
-        back.disabled = true;
-    }
-    txtLabel.innerText = `${current + 1}/${probleme.length}`;
-    rawCode = probleme[curent].text;
+// back.addEventListener("click", function()
+// {
+//     curent--;
+//     if(current == 0)
+//     {
+//         back.disabled = true;
+//     }
+//     txtLabel.innerText = `${current + 1}/${probleme.length}`;
+//     rawCode = probleme[curent].text;
 
-    highlightSyntax(rawCode);
-});
+//     highlightSyntax(rawCode);
+// });
 
-fwd.addEventListener("click", function()
-{
-    curent++;
-    if(current == probleme.length - 1)
-    {
-        fwd.disabled = true;
-    }
-    txtLabel.innerText = `${current + 1}/${probleme.length}`;
-    rawCode = probleme[curent];
+// fwd.addEventListener("click", function()
+// {
+//     curent++;
+//     if(current == probleme.length - 1)
+//     {
+//         fwd.disabled = true;
+//     }
+//     txtLabel.innerText = `${current + 1}/${probleme.length}`;
+//     rawCode = probleme[curent];
 
-    highlightSyntax(rawCode);
-});
+//     highlightSyntax(rawCode);
+// });
 
 function loaded()
 {
